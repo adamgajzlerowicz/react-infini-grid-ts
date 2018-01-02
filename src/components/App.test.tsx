d09474c11873c853b3a6a4006d11fad7f4c73f82
Adam Gajzlerowicz <nelf86@gmail.com>
2018-01-02 21:09:37 +0100

    cleanup

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import { mount, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
-
-configure({ adapter: new Adapter() });
 
 describe('App', () => {
   

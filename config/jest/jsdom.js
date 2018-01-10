d1e2805d6f068f220ce129080544c3bcf2647da3
Adam Gajzlerowicz <nelf86@gmail.com>
2018-01-10 10:59:53 +0100

    updated test suite

--- config/jest/jsdom.js
+++ config/jest/jsdom.js
@@ -1,8 +1,16 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const { document } = new JSDOM(
+const window = new JSDOM(
   `<!doctype html><html><body><div id='root'></div></body></html>`
 ).window;
 
-global.document = document;
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: "node.js"
+};
+
+global.requestAnimationFrame = function(cb) {
+  return setTimeout(cb, 0);
+};
